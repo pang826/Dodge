318cc83ed43992863bc34ddeeacefa9c045e516d
ogh826 <eys555@naver.com>
2024-08-28 11:52:24 +0900

    Feat : 플레이어 움직임 구현
    리지드바디를 통한 움직임을 구현

---
+++ Dodge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 인스펙터 창에서 참조하거나 값을 지정해주어서 쓰는 경우
+    [SerializeField] Rigidbody rigid;
+    [SerializeField] float moveSpeed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        // 입력매니저를 통해 움직임 입력값을 저장
+        float X = Input.GetAxisRaw("Horizontal");
+        float Z = Input.GetAxisRaw("Vertical");
+        // 리지드바디 : 물리엔진을 담당하는 컴포넌트
+        rigid.velocity = new Vector3(X * moveSpeed, 0, Z * moveSpeed);
+    }
+}
